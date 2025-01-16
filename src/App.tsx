2a7f02f954f0084efc28a591b95a6c481b23d004
DESKTOP-TUFTJAJ\dell <asalip.putra@gmail.com>
2025-01-16 22:14:37 +0700

    add conditional builder in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ConditionBuilder } from './components/ConditionBuilder';
+import { initialState } from './contants';
 import './index.scss';
 import * as React from 'react';
 
 const App = () : React.JSX.Element => {
+  const [rule, setRule] = React.useState(initialState) 
   return (
     <div className="App">
-      ok
+      <ConditionBuilder rule={rule} />
     </div>
   );
 }
