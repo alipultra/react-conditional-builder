67e4b6debb547a88fca1a2d1d92d9861f2a5308e
alipultra <asalip.putra@gmail.com>
2025-03-24 15:26:15 +0700

    add context api

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { ConditionBuilder } from './components/ConditionBuilder';
 import { initialState } from './contants';
 import './index.scss';
 import * as React from 'react';
+import { ThemeProvider } from './ThemeContext';
 
 const App = () : React.JSX.Element => {
-  const [rule, setRule] = React.useState(initialState) 
+  const [rule, setRule] = React.useState(initialState)
+
+  const updateRule = (condition) => {
+    setRule(prevState => ({
+      ...prevState,
+      condition,
+    }))
+  }
+
   return (
-    <div className="App">
-      <ConditionBuilder rule={rule} />
-    </div>
+    <ThemeProvider>
+      <ConditionBuilder rule={rule} updateRule={updateRule} />
+    </ThemeProvider>
   );
 }
 
